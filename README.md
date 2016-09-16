f46899a1747e631ed8482c53b5a620daf080a154
Linus Lee <linus.lee@spensatech.com>
2016-09-15 22:37:27 -0400

    Recover from repo damage

---
+++ README.md
@@ -0,0 +1,17 @@
+TEDxPurdueU Website
+
+# Sections
+1. Technologies
+2. Server administration
+3. Mobile platforms
+
+# Technologies
+
+Because our web application hosts mostly static content, until such time as we need more dynamic data interactions, we're using static pages for the main site at `tedxpurdueu.com` and serving static JSON through node at the API endpoints for mobile applications.
+
+
+
+# Server administration
+
+Both our static web server and our JSON (mobile application backend) server are hosted on a Fedora server on DigitalOcean
+
---
+++ js/main.js
@@ -0,0 +1,73 @@
+// page transitions
+setTimeout( function() {
+    $("main").addClass("shown");
+    $("footer").addClass("shown");
+}, 120);
+
+$("a").each(function(i, $el) {
+    $el.setAttribute("data-href", $el.href);
+    $el.setAttribute("href", "//:0");
+});
+
+$("a").each(function(i, $el) {
+    $el.addEventListener("click", function(e) {
+        e.preventDefault();
+        
+        $("header")[0].classList.remove("open");
+
+        if ($el.getAttribute("data-href") != window.location.href) {
+            $("main").removeClass("shown");
+            $(".hero").removeClass("shown").addClass("fasthide");
+            $("footer").removeClass("shown");
+            $("header")[0].className = "";
+
+            setTimeout(function() {
+                window.location.href = $el.getAttribute("data-href");
+            }, 200);
+        } else if (document.body.scrollTop != 0) {
+            scrollToTop(200);
+        }
+    });
+});
+
+function scrollToTop(milliseconds) {
+    var currentPos = document.body.scrollTop;
+    var counter = 0;
+
+    var scrollInterval = setInterval(function(){
+        document.body.scrollTop = currentPos * (milliseconds / 10 - counter) / milliseconds * 10;
+        counter ++;
+
+        if (counter > milliseconds / 10) clearInterval(scrollInterval);
+    }, 10)
+}
+
+// set highlighted links
+$("#menubar a").click(function(evt) {
+    evt.target.className = "active";
+});
+$("a[data-href='" + location.href + "']").addClass("active");
+
+// shuffling images
+$(".hero img").load(function() {
+    $(".hero").addClass("shown");
+});
+
+var id = Math.floor(Math.random() * 11).toString();
+$(".hero img")[0].src = "/assets/" + id + ".jpg";
+
+
+// menu button
+var menubutton = $(".overflowButton")[0],
+    header = $("header")[0];
+menubutton.addEventListener("click", function(evt) {
+    var isClosed = header.className.indexOf("open") == -1;
+
+    console.info(isClosed);
+    if (isClosed) {
+        header.classList.add("open");
+    } else {
+        header.classList.remove("open");
+    }
+});
+
